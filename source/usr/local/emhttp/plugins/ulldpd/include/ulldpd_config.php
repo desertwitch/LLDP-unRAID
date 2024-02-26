ee9d5eb90bb90bbed681a070de51525c79785c03
desertwitch <24509509+desertwitch@users.noreply.github.com>
2024-02-26 11:11:56 +0100

    changes

--- source/usr/local/emhttp/plugins/ulldpd/include/ulldpd_config.php
+++ source/usr/local/emhttp/plugins/ulldpd/include/ulldpd_config.php
@@ -2,4 +2,5 @@
 $ulldpd_cfg          = parse_ini_file("/boot/config/plugins/ulldpd/ulldpd.cfg");
 $ulldpd_service      = isset($ulldpd_cfg['SERVICE'])      ? htmlspecialchars($ulldpd_cfg['SERVICE'])       : 'disable';
 $ulldpd_running      = (intval(trim(shell_exec( "[ -f /proc/`cat /run/lldpd/pid 2> /dev/null`/exe ] && echo 1 || echo 0 2> /dev/null" ))) === 1 );
+$ulldpd_installed_backend = trim(shell_exec("find /var/log/packages/ -type f -iname 'lldpd*' ! -iname 'ulldpd*' -printf '%f\n' 2> /dev/null"));
 ?>
