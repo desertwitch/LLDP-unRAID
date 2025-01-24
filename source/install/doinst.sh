4e47aff8d0752bd27a491fa8b6f44c5b9d2ae52e
desertwitch <24509509+desertwitch@users.noreply.github.com>
2025-01-24 08:48:56 +0100

    changes

--- source/install/doinst.sh
+++ source/install/doinst.sh
@@ -16,15 +16,6 @@
 BOOT="/boot/config/plugins/ulldpd"
 DOCROOT="/usr/local/emhttp/plugins/ulldpd"
 
-# Add LLDPD group and user
-if [ "$( grep -ic "347" /etc/group )" -eq 0 ]; then
-    groupadd -g 347 _lldpd >/dev/null 2>&1
-fi
-
-if [ "$( grep -ic "347" /etc/passwd )" -eq 0 ]; then
-    useradd -d /run/lldpd -s /bin/false -u 347 -g _lldpd _lldpd >/dev/null 2>&1
-fi
-
 # Copy the default configuration file
 cp -n $DOCROOT/default.cfg $BOOT/ulldpd.cfg >/dev/null 2>&1
 
